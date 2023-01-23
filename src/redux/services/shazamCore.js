2e0cb1d61116e3405035edf279968d9390df5776
UmmuhanKavas <ummuhankavas@gmail.com>
2023-01-23 10:49:12 +0100

    uodated

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -7,7 +7,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
     baseQuery: fetchBaseQuery({
         baseUrl:'https://shazam-core.p.rapidapi.com/v1',
         prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key', import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
+            headers.set('X-RapidAPI-Key', '2cd4bcbe3cmsh2e4375d4f2ba8a9p186e27jsn2a5d357df65d');
 
             return headers;
         },
